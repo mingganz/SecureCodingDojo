3aaeaa6a2cca98e550ef8e6b32c394b90ece757e
Navpreet Kaur <Navpreet@aedo.ai>
2021-03-19 08:24:25 -0400

    Issue#1
    solution visible via link injection

--- trainingportal/static/solutionCtrl.js
+++ trainingportal/static/solutionCtrl.js
@@ -1,4 +1,10 @@
 app.controller("solutionCtrl", function($scope, $http, $routeParams) {
+    $http.get('/public/securedojodbdata')
+    .then((response) => {
+        if(response != null && response.data != null){
+            $scope.solutionvisibilitypermissions = Boolean(response.data);
+        }
+    });
     var challengeId = $routeParams.challengeId;
     $scope.solutionLink = "challenges/solutions/"+challengeId;
 });
